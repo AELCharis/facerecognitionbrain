de5f2f000e8482e07d86262832fc3c6487b95d33
AELCharis <AELCharis>
2019-02-25 20:33:28 +0200

    front end functionality

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+                <div className='white f3 '>
+                    {'Charis your current rank is...'}
+                </div>
+            <div className='white f3 '>
+                {'#3'}
+            </div>
+        </div>
+    )
+}
+
+
+export default Rank;
+
